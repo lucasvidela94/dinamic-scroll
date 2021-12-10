416d7349ec5fddf46a7d3cb3d34114d8ac6e5611
lucasvidela94 <69171039+lucasvidela94@users.noreply.github.com>
2021-12-10 19:30:34 -0300

    creada funcion setAttributes que ayuda en la creacion de elementos html

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ const count = 10;
 const apiKey = 'KmFwSgqjd09BQQB5EKEvk-6iTAdvkRj-AKbZbqZjecg';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
+//Crear funcion que ayude a setear atributos
+
+function setAttributes(element , atributtes) {
+    for (const key in atributtes){
+        element.setAttribute(key , atributtes[key]);
+    }
+}
+
+
 //Crear elementos para links y fotos.Al resultado añadirlo al DOM
 
 function showPhotos(){
@@ -18,14 +27,19 @@ function showPhotos(){
     photosArray.forEach((photo) => {
         //Crea un elemento a para linkear con Unsplash
         const item = document.createElement('a');
-        item.setAttribute('href' , photo.links.html);
-        item.setAttribute('target' , '_blank');
+        setAttributes(item , {
+            href  : photo.links.html ,
+            target: '_blank',
+        });
 
         //Crea un elemento img para la foto 
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular );
-        img.setAttribute('alt' , photo.alt_description);
-        img.setAttribute('title' ,photo.alt_description);
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+
         //Poner el elemento img dentro de el elemennto A y todo eso ponerlo dentro de imageContainer(contenedor de imagenes)
         item.appendChild(img);
         imageContainer.appendChild(item)
